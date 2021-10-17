f848a3fe63b9167c9f71151da996ea61b2c19039
Ariful Islam Arif <arif14arif15@gmail.com>
2021-10-17 18:05:39 +0600

    i18n integrate

---
+++ pages/test.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <div>
+      <p>Current language: {{$i18n.locale }}</p>
+
+      <select v-model="selectedValue" @change="onChange(selectedValue)">
+        <option disabled value>Please select one</option>
+        <option
+          v-for="(locale, index) in $i18n.locales"
+          :key="index"
+          :value="locale.code"
+        >{{locale.name}}</option>
+      </select>
+
+      <h2 class="subtitle">{{ $t('welcome') }}</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedValue: ""
+    };
+  },
+  created() {
+    this.selectedValue = this.$i18n.locale;
+  },
+  methods: {
+    onChange(event) {
+      this.$router.replace(this.switchLocalePath(event));
+    }
+  }
+};
+</script>
+
+<style scoped>
+.subtitle {
+  font-weight: 300;
+  font-size: 3em;
+  color: #2E495E;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+  margin-top: 200px;
+}
+</style>
